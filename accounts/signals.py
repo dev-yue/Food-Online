28cd72a73ab075b45fb8dfda3afb0b5c2b24cc9c
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-01-29 15:55:38 -0600

    added user registration and django messages

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,19 +6,16 @@ from .models import User, UserProfile
 def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        print('userProfile is created')
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
-            print('User is updated')
         except:
             # Create the userprofile if not exist
             UserProfile.objects.create(user=instance)
-            print('userProfile was not existed, but I created one.')
-        print('User is updated')
+
 # post_save.connect(post_save_create_profile_receiver, sender=User)
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'this user is being saved')
+    pass
