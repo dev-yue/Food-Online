83ee19262534ec03548e3a8eba21f7b658c39948
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-01-29 20:27:45 -0600

    added vendor registration and authentication for both customer and vendor

--- accounts/models.py
+++ accounts/models.py
@@ -37,11 +37,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
     
     ROLE_CHOICE = (
-        (RESTAURANT, 'restaurant'),
+        (VENDOR, 'Vendor'),
         (CUSTOMER, 'Customer'),
     )
     first_name = models.CharField(max_length=50)
@@ -74,6 +74,13 @@ class User(AbstractBaseUser):
     
     def has_module_perms(self, app_label):
         return True
+    
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
     
 
 class UserProfile(models.Model):
