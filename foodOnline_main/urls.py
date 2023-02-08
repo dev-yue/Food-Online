5e80d9ba16d56d391fff189022be0f65ca004165
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-02-07 23:52:59 -0600

    added snart searcg functionalities

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     
     # Cart
     path('cart/', MarketplaceViews.cart, name='cart'),
+    # Search
+    path('search/', MarketplaceViews.search, name='search'),
      
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
